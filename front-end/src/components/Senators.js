01e93f8fe730ab79ec699d63a4a69f052717bb8b
Matthew Doubet <matthewdoubet@Matthews-MacBook-Air.local>
2018-04-21 13:35:46 -0600

    1st deploy with netlify

--- front-end/src/components/Senators.js
+++ front-end/src/components/Senators.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import senators from '../data/senators'
-import {getSenators} from "../data/mongodb";
+import {getSenators, postSenators} from "../data/mongodb";
 
 import Senator from './Senator'
 
 export default class extends Component {
   state = {
-    senators: ""
+    senate: "", postMessage:""
   };
   renderSenators () {
     return senators.map(senator =>
@@ -14,19 +14,29 @@ export default class extends Component {
     )
   }
 
-  async componentWillMount() {
+  async componentWillUpdate() {
     const mongoFetch = await getSenators();
     const senators = Object.values(mongoFetch)
     this.setState({senate: senators.map(senator => <Senator senator={senator}/>)});
-    
+
   }
 
+  postSenate = async ()=> {
+      postSenators(senators).then(
+      this.setState({postMessage: "Successful Senators"}));
+  };
+
+
+
   render () {
-    const senators = this.renderSenators()
+    // const senators = this.renderSenators()
     
     return (
       <div>
+          <button onClick = {this.postSenate}>Post Senate</button>
         {this.state.senate}
+        <hr/>
+          {this.state.postMessage}
       </div>
     );
   }
--- front-end/src/data/mongodb.js
+++ front-end/src/data/mongodb.js
@@ -1,4 +1,4 @@
-const hapi_url = 'http://localhost:8811';
+const hapi_url = 'https://fullstack-senators-example-frytudbvft.now.sh';
 const senate_route = '/v1/senators';
 
 export const getSenators = async () => {
@@ -8,6 +8,20 @@ export const getSenators = async () => {
         headers: new Headers({
             'Content-Type': 'application/json'
         })
+    }).then(response => results = response.json())
+        .catch(error => console.log("Error: ", error))
+        .then(response => console.log("Success: ", response));
+    return results;
+};
+
+export const postSenators = async (senators) => {
+    let results = "something went wrong in the POST process";
+    await fetch(hapi_url + senate_route, {
+        method: "POST",
+        headers: new Headers({
+            'Content-Type': 'application/json'
+        }),
+        body: JSON.stringify(senators)
     }).then(response => results = response.json())
         .catch(error => console.log("Error: ", error))
         .then(response => console.log("Success: ", response));
